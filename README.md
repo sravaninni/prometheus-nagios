d2d14b66666942502237e4559b7e0f8758a025e6
sravaninni <142392257+sravaninni@users.noreply.github.com>
2024-12-04 14:53:42 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,91 @@
+[![Build Status](https://travis-ci.org/m-lab/prometheus-nagios-exporter.svg?branch=master)](https://travis-ci.org/m-lab/prometheus-nagios-exporter)
+[![Coverage Status](https://coveralls.io/repos/github/m-lab/prometheus-nagios-exporter/badge.svg?branch=master)](https://coveralls.io/github/m-lab/prometheus-nagios-exporter?branch=master)
+
+# prometheus-nagios-exporter
+
+The Prometheus Nagios exporter reads status and performance data from nagios
+plugins via the [MK Livestatus][livestatus] Nagios plugin and publishes this
+in a form that can be scrapped by Prometheus.
+
+[livestatus]: https://mathias-kettner.de/checkmk_livestatus.html
+
+# Setup
+
+Setup is as simple as installing the livestatus module and then running the
+`nagios_exporter.py` service.
+
+    echo 'broker_module=/usr/lib/check_mk/livestatus.o /var/lib/nagios3/rw/livestatus' >> /etc/nagios3/nagios.cfg
+    echo 'event_broker_options=-1' >> /etc/nagios3/nagios.cfg
+
+Restart Nagios, and start the exporter:
+
+    ./nagios_exporter.py --path /var/lib/nagios3/rw/livestatus
+
+It should then be possible to visit:
+
+    http://localhost:9267/metrics
+
+# Metrics
+
+Every metric is prefixed with `nagios_`, following the [metric naming best
+practices][naming]. The prefix is followed by the name of the Nagios check
+command, such as `nagios_check_load_`. The metric name suffix comes from various
+Nagios status names. For example, a load service check for `localhost` would
+include the following metrics:
+
+```
+nagios_check_load_exec_time{hostname="localhost", service="Load"} 0.011084
+nagios_check_load_latency{hostname="localhost", service="Load"} 0.078
+nagios_check_load_state{hostname="localhost", service="Load"} 0
+nagios_check_load_flapping{hostname="localhost", service="Load"} 0
+nagios_check_load_acknowledged{hostname="localhost", service="Load"} 0
+```
+
+Every metric is also labeled with the hostname and service description.
+
+[naming]: https://prometheus.io/docs/practices/naming/
+
+# Performance data
+
+Performance data is plugin-specific. Though there is a common format that most
+plugins follow. Performance data follows plugin output starting with `|`.
+Typically, the format is a set of `key=value1[;value2]+` strings. For example:
+
+```
+$ check_disk <some args>
+DISK OK - free space: / 2400 MB (69% inode=83%);| /=2400MB;48356;54400;0;60445
+```
+
+By default, nagios-exporter only parses the first value of performance data
+for every key. The default field name is 'value'. And, the key is always added
+as a metric label. So, for example, the default metric output for the above
+performance data would be:
+
+```
+nagios_check_disk_perf_data_value{key="/", ...} 2516582400.0
+```
+
+More specific names can be assigned to each value position of particular check
+plugins using the `--data_names` flag. For example:
+
+```
+--data_names="check_disks=used;free;;;total"
+```
+
+So, instead of only parsing the first value and using the default name, now the
+metric output for the original example will include three values each named and
+corresponding to the respective value in the raw perf data:
+
+```
+nagios_check_all_disks_perf_data_used{key="/", ...}  2516582400.0
+nagios_check_all_disks_perf_data_free{key="/", ...} 50704941056.0
+nagios_check_all_disks_perf_data_totalkey="/", ...} 63381176320.0
+```
+
+# Example
+
+```
+    ./nagios_exporter.py --path /var/lib/nagios3/rw/livestatus \
+        --perf_data --perf_names="check_disks=used;free;;;total" \
+        --whitelist nagios_check_all_disks_perf_data
+```
---
+++ nagios_exporter_test.py
@@ -0,0 +1,339 @@
+#!/usr/bin/python
+
+import contextlib
+import io
+import json
+import mock
+import os
+import socket
+import textwrap
+import unittest
+import sys
+from collections import OrderedDict
+
+import nagios_exporter
+
+
+class FakeSocketIO(object):
+    """An in-memory, socket-like object for unit tests."""
+
+    def __init__(self, initial_value=''):
+        if sys.version_info[0] >= 3:
+            initial_value = initial_value.encode()
+
+        self._writer = io.BytesIO()
+        self._reader = io.BytesIO(initial_value)
+
+    def shutdown(self, unused_path):
+        pass
+
+    def close(self):
+        pass
+
+    def recv(self, count=-1):
+        """Reads count bytes from socket, or until EOF when count is -1."""
+        return self._reader.read(count)
+
+    def sendall(self, message):
+        """Writes message to socket."""
+        return self._writer.write(message)
+
+
+def fixed16(code, length):
+    """Creates a fixed16 header."""
+    return "%s %11d\n" % (code, length)
+
+
+class NagiosExporterTest(unittest.TestCase):
+
+    def setUp(self):
+        # A sample service record.
+        self.services = [
+            list(nagios_exporter.Service(
+                host_name='localhost',
+                service_description='Current Load',
+                state=0,
+                latency=0.078,
+                process_performance_data=1,
+                perf_data='load1=0.560;5.000;10.000;0;',
+                check_command='check_load!5.0!4.0!3.0!10.0!6.0!4.0',
+                acknowledged=0,
+                execution_time=0.011084,
+                is_flapping=0
+            ))
+        ]
+
+    def assertItemsEqual(self, *args, **kwargs):
+        # In Python 3, assertItemsEqual is named assertCountEqual
+        # https://hg.python.org/cpython/rev/d9921cb6e3cd
+        if sys.version_info[0] >= 3:
+            return super(NagiosExporterTest, self).assertCountEqual(*args, **kwargs)
+        else:
+            return super(NagiosExporterTest, self).assertItemsEqual(*args, **kwargs)
+
+    def test_canonical_command_with_nrpe(self):
+        actual = nagios_exporter.canonical_command('check_nrpe2!check_node')
+
+        self.assertEqual(actual, 'check_node')
+
+    def test_connect_when_connection_fails(self):
+        with self.assertRaises(nagios_exporter.NagiosConnectError):
+            session = nagios_exporter.connect('/not-a-real-path')
+
+    def test_livestatus_query(self):
+        json_response = json.dumps(self.services)
+        fixed16_header = fixed16('200', len(json_response))
+        fake_sock = FakeSocketIO(fixed16_header + json_response)
+
+        session = nagios_exporter.LiveStatus(fake_sock)
+        actual = session.query('blah')
+
+        self.assertEqual(actual, self.services)
+
+    def test_livestatus_query_when_recv_response_is_empty(self):
+        with self.assertRaises(nagios_exporter.NagiosResponseError):
+            fake_sock = FakeSocketIO('')
+
+            session = nagios_exporter.LiveStatus(fake_sock)
+            session.query('blah')
+
+    def test_livestatus_query_when_recv_raises_exception(self):
+        class FakeSocketIOWithError(FakeSocketIO):
+            """Subclass of FakeSocketIO that raises an exception on sendall"""
+
+            def recv(self, count):
+                raise socket.error('fake socket error')
+
+        with self.assertRaises(nagios_exporter.NagiosResponseError):
+            # Expect 10 bytes that are never sent.
+            fake_sock = FakeSocketIOWithError(fixed16('400', 10))
+
+            session = nagios_exporter.LiveStatus(fake_sock)
+            session.query('blah')
+
+    def test_livestatus_query_when_response_code_is_error(self):
+        with self.assertRaises(nagios_exporter.NagiosResponseError):
+            # Expect 10 bytes that are never sent.
+            message = 'error message'
+            fake_sock = FakeSocketIO(fixed16('400', len(message)) + message)
+
+            session = nagios_exporter.LiveStatus(fake_sock)
+            session.query('blah')
+
+    def test_livestatus_query_when_sendall_raises_exception(self):
+        class FakeSocketIOWithError(FakeSocketIO):
+            """Subclass of FakeSocketIO that raises an exception on sendall"""
+
+            def sendall(self, msg):
+                raise socket.error('fake socket error')
+
+        with self.assertRaises(nagios_exporter.NagiosQueryError):
+            # Text is never used.
+            fake_sock = FakeSocketIOWithError('')
+
+            session = nagios_exporter.LiveStatus(fake_sock)
+            session.query('blah')
+
+    def test_get_services(self):
+        expected = [
+            'nagios_check_load_exec_time{hostname="localhost", service="Current Load"} 0.011084',
+            'nagios_check_load_latency{hostname="localhost", service="Current Load"} 0.078',
+            'nagios_check_load_state{hostname="localhost", service="Current Load"} 0',
+            'nagios_check_load_flapping{hostname="localhost", service="Current Load"} 0',
+            'nagios_check_load_acknowledged{hostname="localhost", service="Current Load"} 0'
+        ]
+        json_response = json.dumps(self.services)
+        fixed16_header = fixed16('200', len(json_response))
+        fake_sock = FakeSocketIO(fixed16_header + json_response)
+
+        session = nagios_exporter.LiveStatus(fake_sock)
+        actual = nagios_exporter.get_services(session, False, ())
+
+        self.assertEqual(actual, expected)
+
+    def test_get_status(self):
+        expected = [
+            'nagios_thing_a 1',
+            'nagios_thing_b 0'
+        ]
+        json_response = json.dumps([['thing_a', 'thing_b'], [1, 0]])
+        fixed16_header = fixed16('200', len(json_response))
+        fake_sock = FakeSocketIO(fixed16_header + json_response)
+
+        session = nagios_exporter.LiveStatus(fake_sock)
+        actual = nagios_exporter.get_status(session)
+
+        self.assertItemsEqual(actual, expected)
+
+    def test_parse_args(self):
+        args = nagios_exporter.parse_args(['--path', '/some/path'])
+
+        self.assertEqual(args.path, '/some/path')
+
+    def test_collect_metrics_with_bad_path(self):
+        args = nagios_exporter.parse_args(['--path', '/not-a-real/path'])
+
+        lines = []
+        nagios_exporter.collect_metrics(args, lines)
+
+        self.assertIn('nagios_livestatus_available 0', lines)
+
+    def test_format_metric_with_various_value_types(self):
+      # Integer.
+      self.assertEqual(
+          'nagios_check_cmd{key="/"} 1',
+          nagios_exporter.format_metric('check_cmd', {'key': '/'}, '1'))
+      # Float.
+      self.assertEqual(
+          'nagios_check_cmd{key="/"} 0.1',
+          nagios_exporter.format_metric('check_cmd', {'key': '/'}, '0.1'))
+      # String.
+      self.assertEqual(
+          'nagios_check_cmd{key="/", value="v0.1"} 1',
+          nagios_exporter.format_metric('check_cmd', OrderedDict({'key': '/'}), 'v0.1'))
+
+    @mock.patch.object(nagios_exporter, 'collect_metrics')
+    def test_metrics_when_exception_is_raised(self, mock_metrics):
+        mock_metrics.side_effect = nagios_exporter.NagiosResponseError('error')
+
+        actual = nagios_exporter.metrics(())
+
+        self.assertEqual(actual.status, '200 OK')
+        self.assertEqual(actual.get_data(as_text=True), 'nagios_exporter_success 0\n')
+
+    @mock.patch.object(nagios_exporter, 'collect_metrics')
+    def test_metrics(self, mock_metrics):
+        actual = nagios_exporter.metrics(())
+
+        self.assertEqual(actual.status, '200 OK')
+        self.assertEqual(actual.get_data(as_text=True), 'nagios_exporter_success 1\n')
+
+    @mock.patch.object(nagios_exporter, 'connect')
+    @mock.patch.object(os.path, 'exists')
+    def test_collect_metrics_when_all_metrics_is_true(
+        self, mock_exists, mock_connect):
+        args = nagios_exporter.parse_args(
+            ['--path', '/not-a-real/path', '--all_metrics', '--perf_data'])
+        expected_status = [
+            'nagios_livestatus_available 1',
+            'nagios_thing_a 1',
+            'nagios_thing_b 0'
+        ]
+        # Setup fake get_status response.
+        json_response = json.dumps([['thing_a', 'thing_b'], [1, 0]])
+        fixed16_header = fixed16('200', len(json_response))
+        fake_status_sock = FakeSocketIO(fixed16_header + json_response)
+        expected_services = [
+            'nagios_check_load_exec_time{hostname="localhost", service="Current Load"} 0.011084',
+            'nagios_check_load_latency{hostname="localhost", service="Current Load"} 0.078',
+            'nagios_check_load_state{hostname="localhost", service="Current Load"} 0',
+            'nagios_check_load_flapping{hostname="localhost", service="Current Load"} 0',
+            'nagios_check_load_acknowledged{hostname="localhost", service="Current Load"} 0',
+            'nagios_check_load_perf_data_value{hostname="localhost", key="load1", service="Current Load"} 0.560'
+        ]
+        # Setup fake get_services response.
+        json_response = json.dumps(self.services)
+        fixed16_header = fixed16('200', len(json_response))
+        fake_service_sock = FakeSocketIO(fixed16_header + json_response)
+        mock_exists.return_value = True
+        mock_connect.side_effect = [fake_status_sock, fake_service_sock]
+
+        values = []
+        nagios_exporter.collect_metrics(args, values)
+
+        self.assertItemsEqual(values, expected_status + expected_services)
+
+    @mock.patch.object(nagios_exporter, 'connect')
+    @mock.patch.object(os.path, 'exists')
+    def test_collect_metrics_when_whitelist(
+        self, mock_exists, mock_connect):
+        args = nagios_exporter.parse_args(
+            ['--path=/not-a-real/path', '--whitelist=nagios_check_load_state'])
+        expected_status = [
+            'nagios_livestatus_available 1',
+            'nagios_thing_a 1',
+            'nagios_thing_b 0'
+        ]
+        # Setup fake get_status response.
+        json_response = json.dumps([['thing_a', 'thing_b'], [1, 0]])
+        fixed16_header = fixed16('200', len(json_response))
+        fake_status_sock = FakeSocketIO(fixed16_header + json_response)
+        expected_services = [
+            'nagios_check_load_state{hostname="localhost", service="Current Load"} 0',
+        ]
+        # Setup fake get_services response.
+        json_response = json.dumps(self.services)
+        fixed16_header = fixed16('200', len(json_response))
+        fake_service_sock = FakeSocketIO(fixed16_header + json_response)
+        mock_exists.return_value = True
+        mock_connect.side_effect = [fake_status_sock, fake_service_sock]
+
+        values = []
+        nagios_exporter.collect_metrics(args, values)
+
+        self.assertItemsEqual(values, expected_status + expected_services)
+
+    @mock.patch.object(socket, 'socket')
+    def test_connect(self, mock_socket):
+        mock_conn = mock.Mock()
+        mock_socket.return_value = mock_conn
+
+        nagios_exporter.connect('/fake-path')
+
+        self.assertEqual(mock_socket.call_count, 1)
+        self.assertEqual(mock_conn.connect.call_count, 1)
+
+    def test_parse_value_and_unit(self):
+        self.assertEqual(
+            nagios_exporter.parse_value_and_unit('2400MB'), ('2400', 'MB'))
+        self.assertEqual(
+            nagios_exporter.parse_value_and_unit('30%'), ('30', '%'))
+        self.assertEqual(
+            nagios_exporter.parse_value_and_unit('0.323ms'), ('0.323', 'ms'))
+        self.assertEqual(
+            nagios_exporter.parse_value_and_unit('3.4'), ('3.4', ''))
+        self.assertEqual(
+            nagios_exporter.parse_value_and_unit('v0.3.4'), ('v0.3.4', ''))
+
+    def test_convert_value_to_base_unit(self):
+        # Known unit.
+        self.assertEqual(
+            nagios_exporter.convert_value_to_base_unit('2400', 'KB'),
+            '2457600.0')
+        # No unit.
+        self.assertEqual(
+            nagios_exporter.convert_value_to_base_unit('2400', ''), '2400')
+        # Ignored / unknown unit.
+        self.assertEqual(
+            nagios_exporter.convert_value_to_base_unit('2400', 'hz'), '2400')
+        # Not a numeric value.
+        self.assertEqual(
+            nagios_exporter.convert_value_to_base_unit('v3.1', ''), 'v3.1')
+        # Not a numeric value with a known unit (e.g. coincidence or bad value)
+        self.assertEqual(
+            nagios_exporter.convert_value_to_base_unit('v3.1', 'KB'), 'v3.1KB')
+
+    def test_get_perf_data(self):
+        expected = [
+            ('check_disk_perf_data_used', {'key': '/'}, '2516582400.0'),
+            ('check_disk_perf_data_free', {'key': '/'}, '50704941056.0'),
+            ('check_disk_perf_data_total', {'key': '/'}, '63381176320.0')
+        ]
+
+        actual = nagios_exporter.get_perf_data(
+            'check_disk', {},
+            ['/=2400MB;48356;54400;0;60445'],
+            ['check_disk=used;free;;;total'])
+
+        self.assertItemsEqual(actual, expected)
+
+    def test_parse_perf_data_fields_with_good_values(self):
+        self.assertItemsEqual(
+                {'/': ['2400MB', '48356', '54400', '0', '60445']},
+                nagios_exporter.parse_perf_data_fields(
+                    ['/=2400MB;48356;54400;0;60445']))
+    def test_parse_perf_data_fields_with_bad_values(self):
+        self.assertItemsEqual({}, nagios_exporter.parse_perf_data_fields(['-6]']))
+
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
